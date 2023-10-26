71e65554e863eeaa3e6cd72a01c30a484bffca17
nack1400 <nack1400@naver.com>
2023-10-25 20:49:00 -0700

    Add common model : created, updated

--- common/models.py
+++ common/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class CommonModel(models.Model):
+    """Common Model Definition"""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="%(app_label)s_%(class)s_created",
+    )
+    updated_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="%(app_label)s_%(class)s_updated",
+    )
+
+    class Meta:
+        abstract = True
