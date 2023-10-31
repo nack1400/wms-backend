96ff9b083be6bf4c31a75be2e5b3184b8807eefc
nack1400 <nack1400@naver.com>
2023-10-31 08:19:26 -0700

    test @swagger_auto_schema

--- categories/views.py
+++ categories/views.py
@@ -4,14 +4,35 @@ from rest_framework import status
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework.exceptions import NotFound
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 class CategoryList(APIView):
+    @swagger_auto_schema(
+        responses={status.HTTP_200_OK: CategorySerializer(many=True)},
+        operation_summary="Get a list of categories",
+        operation_description="Retrieve a list of all categories.",
+    )
     def get(self, request):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "name": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="Category name"
+                ),
+                # Add more fields as needed
+            },
+        ),
+        responses={status.HTTP_201_CREATED: CategorySerializer()},
+        operation_summary="Create a new category",
+        operation_description="Create a new category with the specified data.",
+    )
     def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
